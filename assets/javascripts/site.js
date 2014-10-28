e4049b8b69856642638fff2fe7c01a408e908714
xiajian <jhqy2011@gmail.com>
2014-10-28 14:36:56 +0800

    添加scrollup插件

---
+++ assets/javascripts/site.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $.scrollUp({
+        scrollName: 'scrollUp',
+        topDistance: '300',
+        topSpeed: 300,
+        animation: 'fade',
+        animationInSpeed: 200,
+        animationOutSpeed: 200,
+        scrollText: '<i class="fa fa-angle-up"></i>',
+        activeOverlay: !1
+    })
+});
+
